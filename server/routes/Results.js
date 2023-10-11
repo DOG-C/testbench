58e26f62cd4bb04118fc877212d15948303f1350
dogc <caoyuqi1996@hotmail.com>
2023-10-11 03:07:25 +0200

    connected server with client

--- server/routes/Results.js
+++ server/routes/Results.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { Results } = require("../models");
 
-router.get("/", (req, res) => {
-    res.send("Hello World!")
-});
+router.get("/", async (req, res) => {
+    const listOfPosts = await Results.findAll();
+    res.json(listOfPosts);
+  });
+  
+  router.post("/", async (req, res) => {
+    const post = req.body;
+    await Results.create(post);
+    res.json(post);
+  });
 
 // A example for post request
 // router.post("/", async (req, res) => {
